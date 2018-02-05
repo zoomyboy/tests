70ce0a01de1bf039cd6f4f661751b1d6be10d40e
Werbeagentur AWEOS <philipp@aweos.de>
2018-02-05 13:07:49 +0100

    Add MigratesDb Trait

---
+++ src/Traits/MigratesDb.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Zoomyboy\Tests\Traits;
+
+class MigratesDb
+{
+    public function runSeeder($seeder)
+    {
+        (new $seeder)->run();
+    }
+
+    public function runMigration($name)
+    {
+        $this->beforeApplicationDestroyed(function () use ($name) {
+            $this->rollbackMigration($name);
+        });
+        $migration = $this->getMigrationInstance($name);
+        $migration->up();
+    }
+
+    public function rollbackMigration($name)
+    {
+        $migration = $this->getMigrationInstance($name);
+        $migration->down();
+    }
+
+    public function getMigrationInstance($name)
+    {
+        $migrator = $this->app->make('migrator');
+        $files = $migrator->getMigrationFiles(base_path('database/migrations'));
+        $migrator->requireFiles($files);
+        $file = array_filter($files, function ($file) use ($name) {
+            return Str::endsWith($file, $name.'.php');
+        });
+        if (!count($file)) {
+            throw new \Exception('Migration '.$name .' nicht gefunden.');
+        }
+        $file = array_shift($file);
+        return $migrator->resolve($migrator->getMigrationName($file));
+    }
+}
