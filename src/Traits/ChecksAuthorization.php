be6393a60e2d672355e236bb4da7e69f7e4ab74c
Philipp Lang <pille@zoomyboy.de>
2018-06-19 00:22:59 +0200

    Add failure messages for authorization

--- src/Traits/ChecksAuthorization.php
+++ src/Traits/ChecksAuthorization.php
@@ -23,18 +23,24 @@ trait ChecksAuthorization {
         return $user;
     }
 
-    public function assertRequestFails($class, $values = [], $messages = null) {
-        $request = new $class([], $values);
+    public function assertValidationFails($class, $values, $with = []) {
+        $request = (new $class())->replace($values);
         $validator = Validator::make($values, $request->rules());
 
-        $this->assertTrue($validator->fails());
+        $this->assertTrue($validator->fails(), 'Validation expected to fail, but it succeeded with Request '.print_r($values, true));
+        $this->assertEmpty(
+            array_diff($with, array_keys($validator->failed())),
+            "Failed asserting that Validation failed with ".implode(', ', $with)."\n
+            Request: ".print_r($values, true)."\n
+            Failed Rules: ".implode(', ', array_keys($validator->failed()))
+        );
     }
 
-    public function assertRequestPasses($class, $values = [], $messages = null) {
-        $request = new $class([], $values);
+    public function assertValidationPasses($class, $values = []) {
+        $request = new $class([], $values, [], [], [], ['REQUEST_METHOD' => 'PATCH']);
         $validator = Validator::make($values, $request->rules());
 
-        $this->assertFalse($validator->fails());
+        $this->assertTrue($validator->passes(), 'Validation expected to pass, but it failed with Request '.print_r($values, true).' and Rules '.print_r($validator->failed(), true));
     }
 
     public function getUserModel() {
