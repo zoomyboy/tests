18d581af5cdee42e24d9f1970cfa764463e1a049
Philipp Lang <pille@zoomyboy.de>
2019-04-03 23:42:43 +0200

    Add api prefix

--- src/Traits/HandlesApiCalls.php
+++ src/Traits/HandlesApiCalls.php
@@ -10,43 +10,43 @@ use Laravel\Passport\Client;
 use Illuminate\Support\Facades\Session;
 
 trait HandlesApiCalls {
-	public $token;
+    public $token;
 
-	public function bootPassport($user = null, $client = 'API') {
-		$this->runMigration('auth_codes_table');
-		$this->runMigration('access_tokens_table');
-		$this->runMigration('refresh_tokens_table');
-		$this->runMigration('oauth_clients_table');
-		$this->runMigration('oauth_personal_access_clients_table');
+    public function bootPassport($user = null, $client = 'API') {
+        $this->runMigration('auth_codes_table');
+        $this->runMigration('access_tokens_table');
+        $this->runMigration('refresh_tokens_table');
+        $this->runMigration('oauth_clients_table');
+        $this->runMigration('oauth_personal_access_clients_table');
 
-		Artisan::call('passport:client', [
-			'--personal' => true,
-			'--name' => $client
-		]);
+        Artisan::call('passport:client', [
+            '--personal' => true,
+            '--name' => $client
+        ]);
 
-		$user = $user ?: $this->create('User');
-		$this->token = $user->createToken($client)->accessToken;
-	}
+        $user = $user ?: $this->create('User');
+        $this->token = $user->createToken($client)->accessToken;
+    }
 
-	public function getApi($to, $data=[]) {
-		return $this->getJson('/api/'.$to, $data);
-	}
+    public function getApi($to, $data=[]) {
+        return $this->getJson($this->apiPrefix.$to, $data);
+    }
 
-	public function visit($url) {
-		return $this->get($url);
-	}
+    public function visit($url) {
+        return $this->get($url);
+    }
 
-	public function postApi($to, $data=[], $headers = []) {
-		return $this->postJson('/api/'.$to, $data, $headers);
-	}
+    public function postApi($to, $data=[], $headers = []) {
+        return $this->postJson($this->apiPrefix.$to, $data, $headers);
+    }
 
-	public function deleteApi($to, $data=[]) {
-		return $this->deleteJson('/api/'.$to, $data);
-	}
+    public function deleteApi($to, $data=[]) {
+        return $this->deleteJson($this->apiPrefix.$to, $data);
+    }
 
-	public function patchApi($to, $data) {
-		return $this->postJson('/api/'.$to, array_merge($data, ['_method' => 'patch']));
-	}	
+    public function patchApi($to, $data) {
+        return $this->postJson($this->apiPrefix.$to, array_merge($data, ['_method' => 'patch']));
+    }
 
     /**
      * Create the test response instance from the given response.
@@ -57,15 +57,15 @@ trait HandlesApiCalls {
     protected function createTestResponse($response)
     {
         return Response::fromBaseResponse($response);
-    }	
+    }
 
-	public function postRealApi($url, $data = [], $token = null) {
-		$token = $token ?: $this->token;
+    public function postRealApi($url, $data = [], $token = null) {
+        $token = $token ?: $this->token;
 
-		$call = $this->postApi($url, $data, [
-			'Authorization' => 'Bearer '.$token
-		]);
+        $call = $this->postApi($url, $data, [
+            'Authorization' => 'Bearer '.$token
+        ]);
 
-		return $call;
-	}
+        return $call;
+    }
 }
