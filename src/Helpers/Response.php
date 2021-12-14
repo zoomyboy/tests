c4e086aeae954996aa355490e63affa431537d19
Philipp Lang <philipp@aweos.de>
2021-12-14 14:09:48 +0100

    Fixed TestResponse namespace

--- src/Helpers/Response.php
+++ src/Helpers/Response.php
@@ -2,40 +2,40 @@
 
 namespace Zoomyboy\Tests\Helpers;
 
-use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Testing\TestResponse;
 
 class Response extends TestResponse {
-	use \Illuminate\Validation\Concerns\FormatsMessages;
+    use \Illuminate\Validation\Concerns\FormatsMessages;
 
-	public $customMessages = [];
+    public $customMessages = [];
 
-	public function assertSuccess() {
-		return $this->assertStatus(200);
-	}
+    public function assertSuccess() {
+        return $this->assertStatus(200);
+    }
 
-	public function assertNotFound() {
-		return $this->assertStatus(404);
-	}
+    public function assertNotFound() {
+        return $this->assertStatus(404);
+    }
 
-	public function assertRedirectTo($url) {
-		return $this->assertRedirect($url);
-	}
+    public function assertRedirectTo($url) {
+        return $this->assertRedirect($url);
+    }
 
-	public function assertRedirectedTo($url) {
-		return $this->assertRedirect($url);
-	}
+    public function assertRedirectedTo($url) {
+        return $this->assertRedirect($url);
+    }
 
-	public function assertForbidden() {
-		return $this->assertStatus(403);
-	}
+    public function assertForbidden() {
+        return $this->assertStatus(403);
+    }
 
-	public function assertUnauthorized() {
-		return $this->assertStatus(401);
-	}
+    public function assertUnauthorized() {
+        return $this->assertStatus(401);
+    }
 
-	public function assertValidationFailedWith(...$field) {
-		$this->assertJsonStructure($field)->assertStatus(422);
+    public function assertValidationFailedWith(...$field) {
+        $this->assertJsonStructure($field)->assertStatus(422);
 
-		return $this;
-	}
-}	
+        return $this;
+    }
+}   
